985a184d06eb51e79fb9bdedc7856c17ad1460a3
Peter Morris <mrpmorris@gmail.com>
2019-08-10 14:30:30 +0100

    Navigating via code

---
+++ NavigatingViaCode/NavigatingViaCode/NavigatingViaCode/Pages/CounterBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Diagnostics;
+
+namespace NavigatingViaCode.Pages
+{
+	public class CounterBase : ComponentBase, IDisposable
+	{
+		// Inject an instance of the IUriHelper
+		[Inject] protected IUriHelper UriHelper { get; set; }
+
+		protected override void OnInit()
+		{
+			// Subscribe to the event
+			UriHelper.OnLocationChanged += LocationChanged;
+			base.OnInit();
+		}
+
+		public void Dispose()
+		{
+			// Unsubscribe from the event when our component is disposed
+			UriHelper.OnLocationChanged -= LocationChanged;
+		}
+
+		private void LocationChanged(object sender, LocationChangedEventArgs e)
+		{
+			string navigationMethod = e.IsNavigationIntercepted ? "HTML" : "code";
+			Debug.WriteLine($"Notified of navigation via {navigationMethod} to {e.Location}");
+		}
+	}
+}
