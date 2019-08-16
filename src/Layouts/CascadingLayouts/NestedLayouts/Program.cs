c68dd6e1f3fcd88115b4d0bbd67e8f082d5e53de
Peter Morris <mrpmorris@gmail.com>
2019-08-16 20:38:27 +0100

    Nested layouts

--- src/Layouts/CascadingLayouts/NestedLayouts/Program.cs
+++ src/Layouts/CascadingLayouts/NestedLayouts/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
-namespace CascadingLayouts
+namespace NestedLayouts
 {
 	public class Program
 	{
--- src/Layouts/CascadingLayouts/NestedLayouts/Startup.cs
+++ src/Layouts/CascadingLayouts/NestedLayouts/Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CascadingLayouts
+namespace NestedLayouts
 {
 	public class Startup
 	{
