58b28424a1c13e5507d611b762b393639db7ed01
Peter Morris <mrpmorris@gmail.com>
2019-08-17 13:44:59 +0100

    Creating a component

---
+++ src/Components/CreatingAComponent/CreatingAComponent/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace CreatingAComponent
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+			BlazorWebAssemblyHost.CreateDefaultBuilder()
+				.UseBlazorStartup<Startup>();
+	}
+}
