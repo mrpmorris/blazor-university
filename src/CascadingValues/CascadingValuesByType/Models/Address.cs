af5a98e356181a0c536f8892545049fced576b9b
Peter Morris <mrpmorris@gmail.com>
2019-08-17 16:04:05 +0100

    Cascading values by type

---
+++ src/CascadingValues/CascadingValuesByType/Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace CascadingValuesByType.Models
+{
+	public class Address
+	{
+		public string Lines { get; set; }
+		public string Area { get; set; }
+		public string Country { get; set; }
+		public string PostalCode { get; set; }
+	}
+}
