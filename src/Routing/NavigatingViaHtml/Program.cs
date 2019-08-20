ca8be5aa226c5b4bc37f9d66ca10dddfa0bb1fda
Peter Morris <mrpmorris@gmail.com>
2019-08-20 22:16:44 +0100

    Routing: Navigating via HTML

--- src/Routing/NavigatingViaHtml/Program.cs
+++ src/Routing/NavigatingViaHtml/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Blazor.Hosting;
 
-namespace NavigationViaHtml
+namespace NavigatingViaHtml
 {
 	public class Program
 	{
--- src/Routing/NavigatingViaHtml/Startup.cs
+++ src/Routing/NavigatingViaHtml/Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace NavigationViaHtml
+namespace NavigatingViaHtml
 {
 	public class Startup
 	{
