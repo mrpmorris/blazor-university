9ebbc8f29bfdaebc22f4ee6e82e3acaa0b11732c
Peter Morris <mrpmorris@gmail.com>
2019-08-27 20:01:53 +0100

    Validation

---
+++ src/Forms/ValidatingUserInput/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidatingUserInput.Models
+{
+	public class Person
+	{
+		[Required]
+		public string Name { get; set; }
+		[Range(18, 80, ErrorMessage = "Age must be between 18 and 80.")]
+		public int Age { get; set; }
+	}
+}
