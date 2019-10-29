f96f0fc9e30366e392dd1448f871aa709b6b5874
Peter Morris <mrpmorris@gmail.com>
2019-10-29 16:42:24 +0000

    Passing complex objects to JavaScript

---
+++ src/JavaScriptInterop/PassingComplexObjectsToJavaScript/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PassingComplexObjectsToJavaScript.Models
+{
+	public class Person
+	{
+		public string Salutation { get; set; }
+		public string GivenName { get; set; }
+		public string FamilyName { get; set; }
+		public List<KeyValuePair<string, string>> PhoneNumbers { get; set; }
+
+		public Person()
+		{
+			PhoneNumbers = new List<KeyValuePair<string, string>>();
+		}
+	}
+}
