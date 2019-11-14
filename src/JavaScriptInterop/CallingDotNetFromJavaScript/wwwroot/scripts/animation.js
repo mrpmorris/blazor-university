8782afcc6769d589beafc97401b30c5fce12b67e
Peter Morris <mrpmorris@gmail.com>
2019-11-14 16:03:48 +0000

    Calling dotnet from JavaScript

---
+++ src/JavaScriptInterop/CallingDotNetFromJavaScript/wwwroot/scripts/animation.js
@@ -0,0 +1,30 @@
+﻿var BlazorUniversity = BlazorUniversity || {};
+BlazorUniversity.animate = function (htmlElement, animationName, duration, dotNetObject) {
+    let animationStartCallback = () => {
+        dotNetObject.invokeMethodAsync('AnimationStarted', animationName);
+    }
+
+    let animationEndCallback = () => {
+        // Code for Chrome, Safari and Opera
+        htmlElement.removeEventListener('webkitAnimationStart', animationStartCallback);
+        htmlElement.removeEventListener('webkitAnimationEnd', animationEndCallback);
+
+        // Standard syntax
+        htmlElement.removeEventListener('animationstart', animationStartCallback);
+        htmlElement.removeEventListener('animationend', animationEndCallback);
+
+        dotNetObject.invokeMethodAsync('AnimationEnded', animationName);
+    }
+
+    // Code for Chrome, Safari and Opera
+    htmlElement.addEventListener('webkitAnimationStart', animationStartCallback);
+    htmlElement.addEventListener('webkitAnimationEnd', animationEndCallback);
+
+    // Standard syntax
+    htmlElement.addEventListener('animationstart', animationStartCallback);
+    htmlElement.addEventListener('animationend', animationEndCallback);
+
+    let animText = `${animationName} ${duration}s`;
+    htmlElement.style.WebkitAnimation = animText;
+    htmlElement.style.animation = animText;
+};
