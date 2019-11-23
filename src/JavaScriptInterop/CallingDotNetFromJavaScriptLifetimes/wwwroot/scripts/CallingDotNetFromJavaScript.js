1bb0c2f5b067cbf94e5e11fc8abebac9fde703d4
Peter Morris <mrpmorris@gmail.com>
2019-11-23 12:02:58 +0000

    Calling DotNet, lifetimes

---
+++ src/JavaScriptInterop/CallingDotNetFromJavaScriptLifetimes/wwwroot/scripts/CallingDotNetFromJavaScript.js
@@ -0,0 +1,8 @@
+﻿var BlazorUniversity = BlazorUniversity || {};
+BlazorUniversity.startRandomGenerator = function(dotNetObject) {
+	setInterval(function () {
+		let text = Math.random() * 1000;
+		console.log("JS: Generated " + text);
+		dotNetObject.invokeMethodAsync('AddText', text.toString());
+	}, 1000);
+};
