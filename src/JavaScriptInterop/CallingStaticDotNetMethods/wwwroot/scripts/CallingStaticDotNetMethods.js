937f4bece8a022b5e0be30b8b9fa593ce8a3b5a9
Peter Morris <mrpmorris@gmail.com>
2019-11-30 16:42:18 +0000

    Invoke static .NET (#8)
    * Calling static methods
    
    * Blazor.start

--- src/JavaScriptInterop/CallingStaticDotNetMethods/wwwroot/scripts/CallingStaticDotNetMethods.js
+++ src/JavaScriptInterop/CallingStaticDotNetMethods/wwwroot/scripts/CallingStaticDotNetMethods.js
@@ -1,4 +1,25 @@
-﻿setTimeout(async function () {
-	let settings = await DotNet.invokeMethodAsync("CallingStaticDotNetMethods", "GetSettings");
-	console.log(settings);
+﻿// Invoke Blazor after 1 second
+setTimeout(async function () {
+	const settings = await DotNet.invokeMethodAsync("CallingStaticDotNetMethods", "GetSettings");
+	alert('API key: ' + settings.someApiKey);
 }, 1000);
+
+// Attempt to invoke Blazor every 10 milliseconds until successful
+//window.someInitialization = async function () {
+//	try {
+//		const settings = await DotNet.invokeMethodAsync("CallingStaticDotNetMethods", "GetSettings");
+//		alert('API key: ' + settings.someApiKey);
+//	}
+//	catch {
+//		// Try again
+//		this.setTimeout(someInitialization, 10);
+//	}
+//}
+//window.someInitialization();
+
+// Use Blazor.start - Don't forget to set autostart="false" in /Pages/_Host.cshtml
+//Blazor.start({})
+//	.then(async function () {
+//		const settings = await DotNet.invokeMethodAsync("CallingStaticDotNetMethods", "GetSettings");
+//		alert('API key: ' + settings.someApiKey);
+//	});
