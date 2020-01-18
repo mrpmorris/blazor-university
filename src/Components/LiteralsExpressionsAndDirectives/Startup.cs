d6868a4fc59d00e020893c6a89a81a0f67c5a5f2
Peter Morris <mrpmorris@gmail.com>
2020-01-18 14:56:06 +0000

    L.E.D. Expressions

--- src/Components/LiteralsExpressionsAndDirectives/Startup.cs
+++ src/Components/LiteralsExpressionsAndDirectives/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using LiteralsExpressionsAndDirectives.Data;
 
 namespace LiteralsExpressionsAndDirectives
 {
@@ -28,7 +27,6 @@ namespace LiteralsExpressionsAndDirectives
 		{
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/Components/LiteralsExpressionsAndDirectives/ViewModels/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace LiteralsExpressionsAndDirectives.ViewModels
+{
+	public class Person
+	{
+		public string Salutation { get; set; }
+		public string GivenName { get; set; }
+		public string FamilyName { get; set; }
+
+		public override string ToString() => $"{Salutation} {GivenName} {FamilyName}";
+	}
+}
