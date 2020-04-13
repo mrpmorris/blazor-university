42dfb65ba3b62f329257c5ddb2fb479483dd0448
Peter Morris <mrpmorris@gmail.com>
2020-04-13 11:53:53 +0100

    Dependency injection - Web chat (#13)

---
+++ src/Configuration/WebAssemblyRemoteConfigurationFiles/Shared/ConfigurationObjects/ServiceSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAssemblyRemoteConfigurationFiles.Shared.ConfigurationObjects
+{
+	public class ServiceSettings
+	{
+		public string Url { get; set; }
+		public string AccessToken { get; set; }
+	}
+}
---
+++ src/DependencyInjection/WebChat/Pages/Index.razor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using WebChat.Services;
+
+namespace WebChat.Pages
+{
+	public partial class Index : IDisposable
+	{
+		[Required(ErrorMessage = "Enter name")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Enter a message")]
+		public string Text { get; set; }
+
+		[Inject]
+		private IChatService ChatService { get; set; }
+		private string[] ChatHistory = Array.Empty<string>();
+		private string ChatHistoryText => string.Join('\r', ChatHistory);
+
+		protected override void OnInitialized()
+		{
+			base.OnInitialized();
+			ChatHistory = ChatService.GetChatHistory();
+			ChatService.TextAdded += TextAdded;
+		}
+
+		private void SendMessage()
+		{
+			if (ChatService.SendMessage(Name, Text))
+				Text = "";
+		}
+
+		private void TextAdded(object sender, string line)
+		{
+			ChatHistory = ChatHistory.Take(49).Append(line).ToArray();
+			InvokeAsync(StateHasChanged);
+		}
+
+		void IDisposable.Dispose()
+		{
+			ChatService.TextAdded -= TextAdded;
+		}
+	}
+}
