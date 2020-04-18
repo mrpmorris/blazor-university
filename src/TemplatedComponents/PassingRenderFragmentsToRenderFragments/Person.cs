f12506025009898d5829103cd867caa29a9af5b5
Peter Morris <mrpmorris@gmail.com>
2020-04-18 10:20:49 +0100

    Generics

---
+++ src/TemplatedComponents/PassingRenderFragmentsToRenderFragments/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace PassingRenderFragmentsToRenderFragments
+{
+	public class Person
+	{
+		public string Salutation { get; set; }
+		public string GivenName { get; set; }
+		public string FamilyName { get; set; }
+	}
+}
---
+++ src/TemplatedComponents/UsingTypeParamToCreateGenericComponents/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace UsingTypeParamToCreateGenericComponents
+{
+	public class Person
+	{
+		public string Salutation { get; set; }
+		public string GivenName { get; set; }
+		public string FamilyName { get; set; }
+	}
+}
