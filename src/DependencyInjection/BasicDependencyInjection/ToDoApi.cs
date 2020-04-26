a04a40d127683747cb55e6d5630e42db45668092
Peter Morris <mrpmorris@gmail.com>
2020-04-26 17:39:20 +0100

    Simple DI sample (#14)

---
+++ src/DependencyInjection/BasicDependencyInjection/ToDoApi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BasicDependencyInjection
+{
+	public interface IToDoApi
+	{
+		Task<IEnumerable<ToDo>> GetToDosAsync();
+	}
+
+	public class ToDoApi : IToDoApi
+	{
+		private readonly IEnumerable<ToDo> Data;
+
+		public ToDoApi()
+		{
+			Data = new ToDo[]
+			{
+				new ToDo { Id = 1, Title = "To do 1", Completed = true},
+				new ToDo { Id = 2, Title = "To do 2", Completed = false},
+				new ToDo { Id = 3, Title = "To do 3", Completed = false},
+			};
+		}
+
+		public Task<IEnumerable<ToDo>> GetToDosAsync() => Task.FromResult(Data);
+	}
+}
