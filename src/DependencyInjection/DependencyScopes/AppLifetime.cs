c171571b60bbfc9484300353da12a63ad7ff9007
Peter Morris <mrpmorris@gmail.com>
2020-04-28 16:23:23 +0100

    New instance labels

---
+++ src/DependencyInjection/DependencyScopes/AppLifetime.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DependencyScopes
+{
+	public static class AppLifetime
+	{
+		public static DateTime StartTimeUtc { get; } = DateTime.UtcNow;
+		public static TimeSpan ElapsedTime => DateTime.UtcNow - StartTimeUtc;
+	}
+}
