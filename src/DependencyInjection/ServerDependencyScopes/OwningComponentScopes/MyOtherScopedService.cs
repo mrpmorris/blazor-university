b30060c510949c7875c88c1ae830fb44545a6dc0
Peter Morris <mrpmorris@gmail.com>
2020-05-31 15:42:57 +0100

    OwningComponentBase (#21)
    Owning component base

---
+++ src/DependencyInjection/ServerDependencyScopes/OwningComponentScopes/MyOtherScopedService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OwningComponentScopes
+{
+	public interface IMyOtherScopedService
+	{
+		public TimeSpan DeltaCreationTime { get; }
+		public int InstanceNumber { get; }
+	}
+
+
+	public sealed class MyOtherScopedService : IMyOtherScopedService, IDisposable
+	{
+		public TimeSpan DeltaCreationTime { get; }
+		public int InstanceNumber { get; }
+
+		private static volatile int PreviousInstanceNumber;
+
+		public MyOtherScopedService()
+		{
+			DeltaCreationTime = DateTime.UtcNow - AppLifetime.StartTimeUtc;
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+
+		void IDisposable.Dispose()
+		{
+			System.Diagnostics.Debug.WriteLine($"Disposed MyOtherScopedService instance {InstanceNumber}");
+		}
+	}
+
+}
---
+++ src/DependencyInjection/ServerDependencyScopes/OwningComponentScopes/MyScopedService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OwningComponentScopes
+{
+	public interface IMyScopedService
+	{
+		public TimeSpan DeltaCreationTime { get; }
+		public int InstanceNumber { get; }
+		public IMyOtherScopedService MyOtherScopedService { get;  }
+	}
+
+	public sealed class MyScopedService : IMyScopedService, IDisposable
+	{
+		public TimeSpan DeltaCreationTime { get; }
+		public int InstanceNumber { get; }
+		public IMyOtherScopedService MyOtherScopedService { get;  }
+
+		private static volatile int PreviousInstanceNumber;
+
+		public MyScopedService(IMyOtherScopedService myOtherScopedService)
+		{
+			DeltaCreationTime = DateTime.UtcNow - AppLifetime.StartTimeUtc;
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+			MyOtherScopedService = myOtherScopedService;
+		}
+
+		void IDisposable.Dispose()
+		{
+			System.Diagnostics.Debug.WriteLine($"Disposed MyScopedService instance {InstanceNumber}");
+		}
+	}
+}
