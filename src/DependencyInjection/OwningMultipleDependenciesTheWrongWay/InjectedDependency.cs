1ff218a9ddacb78645f709538902116f94431b13
Peter Morris <mrpmorris@gmail.com>
2020-06-14 14:29:25 +0100

    Ignore URL List

---
+++ src/DependencyInjection/OwningMultipleDependenciesTheWrongWay/InjectedDependency.cs
@@ -0,0 +1,19 @@
+﻿namespace OwningMultipleDependenciesTheWrongWay
+{
+	public interface IInjectedDependency
+	{
+		public int InstanceNumber { get; }
+	}
+
+
+	public class InjectedDependency : IInjectedDependency
+	{
+		private static volatile int PreviousInstanceNumber;
+
+		public int InstanceNumber { get; }
+		public InjectedDependency()
+		{
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+	}
+}
---
+++ src/DependencyInjection/OwningMultipleDependenciesTheWrongWay/OwnedDependency.cs
@@ -0,0 +1,19 @@
+﻿namespace OwningMultipleDependenciesTheWrongWay
+{
+	public interface IOwnedDependency
+	{
+		public int InstanceNumber { get; }
+	}
+
+
+	public class OwnedDependency : IOwnedDependency
+	{
+		private static volatile int PreviousInstanceNumber;
+
+		public int InstanceNumber { get; }
+		public OwnedDependency()
+		{
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+	}
+}
