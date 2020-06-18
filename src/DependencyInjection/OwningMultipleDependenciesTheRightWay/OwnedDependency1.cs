d8e83d28d62c0aa9b20af980f80de5830465a165
Peter Morris <mrpmorris@gmail.com>
2020-06-18 22:37:44 +0100

    OwningComponentBase

---
+++ src/DependencyInjection/OwningMultipleDependenciesTheRightWay/OwnedDependency1.cs
@@ -0,0 +1,19 @@
+﻿namespace OwningMultipleDependenciesTheRightWay
+{
+	public interface IOwnedDependency1
+	{
+		public int InstanceNumber { get; }
+	}
+
+
+	public class OwnedDependency1 : IOwnedDependency1
+	{
+		private static volatile int PreviousInstanceNumber;
+
+		public int InstanceNumber { get; }
+		public OwnedDependency1()
+		{
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+	}
+}
---
+++ src/DependencyInjection/OwningMultipleDependenciesTheRightWay/OwnedDependency2.cs
@@ -0,0 +1,19 @@
+﻿namespace OwningMultipleDependenciesTheRightWay
+{
+	public interface IOwnedDependency2
+	{
+		public int InstanceNumber { get; }
+	}
+
+
+	public class OwnedDependency2 : IOwnedDependency2
+	{
+		private static volatile int PreviousInstanceNumber;
+
+		public int InstanceNumber { get; }
+		public OwnedDependency2()
+		{
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+	}
+}
