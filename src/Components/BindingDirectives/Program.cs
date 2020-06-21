8c34c4a253ce09232ee16c7cdcfbd39ad9a4a63b
KP Krishna Kumar <kpkrish@gmail.com>
2020-06-21 10:07:28 +0530

    Reverted Server projects back to original. Bumping up TargetFramework from netcoreapp3.0 to netcoreapp3.1

--- src/Components/BindingDirectives/Program.cs
+++ src/Components/BindingDirectives/Program.cs
@@ -1,21 +1,28 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Net.Http;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace BindingDirectives
 {
-	public static class Program
+	public class Program
 	{
-		public async static Task Main(string[] args)
+		public static void Main(string[] args)
 		{
-			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-			builder.RootComponents.Add<App>("app");
-
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-			await builder.Build().RunAsync();
+			CreateHostBuilder(args).Build().Run();
 		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+				Host.CreateDefaultBuilder(args)
+						.ConfigureWebHostDefaults(webBuilder =>
+						{
+							webBuilder.UseStartup<Startup>();
+						});
 	}
 }
