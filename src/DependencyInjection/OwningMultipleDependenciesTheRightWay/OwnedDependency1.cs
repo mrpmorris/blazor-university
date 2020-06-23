5eac0ec61a3612a2801e7d514e554195f5777a30
Peter Morris <mrpmorris@gmail.com>
2020-06-23 19:31:28 +0100

    Dependency injection - OwningComponentBase the right way (#30)
    * OwningComponentBase
    
    * Write to output window when disposed

---
+++ src/DependencyInjection/OwningMultipleDependenciesTheRightWay/OwnedDependency1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OwningMultipleDependenciesTheRightWay
+{
+	public interface IOwnedDependency1
+	{
+		public int InstanceNumber { get; }
+	}
+
+
+	public class OwnedDependency1 : IOwnedDependency1, IDisposable
+	{
+		private static volatile int PreviousInstanceNumber;
+
+		public int InstanceNumber { get; }
+		public OwnedDependency1()
+		{
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+
+		public void Dispose()
+		{
+			System.Diagnostics.Debug.WriteLine("Disposing " + GetType().Name);
+		}
+	}
+}
---
+++ src/DependencyInjection/OwningMultipleDependenciesTheRightWay/OwnedDependency2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OwningMultipleDependenciesTheRightWay
+{
+	public interface IOwnedDependency2
+	{
+		public int InstanceNumber { get; }
+	}
+
+
+	public class OwnedDependency2 : IOwnedDependency2, IDisposable
+	{
+		private static volatile int PreviousInstanceNumber;
+
+		public int InstanceNumber { get; }
+		public OwnedDependency2()
+		{
+			InstanceNumber = System.Threading.Interlocked.Increment(ref PreviousInstanceNumber);
+		}
+
+		public void Dispose()
+		{
+			System.Diagnostics.Debug.WriteLine("Disposing " + GetType().Name);
+		}
+	}
+}
